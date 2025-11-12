5a018aac634440094cbf25256765fcb31882765f
mohd kaif <mk2818356@gmail.com>
2025-11-12 21:08:37 +0530

    init

---
+++ apps/api/index.ts
@@ -0,0 +1,18 @@
+import express from "express"
+
+const app = express()
+
+app.post("/signup",(req,res)=>{
+
+
+    
+})
+
+
+app.get("/repo",(req,res)=>{
+    
+})
+
+app.listen(8000,()=>{
+    console.log("server is running on port 8000")
+})
