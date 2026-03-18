45a5376fdf63f31990b44fe1f04615daa03dd047
mohd kaif <mk2818356@gmail.com>
2026-03-18 23:51:39 +0530

    socket changes

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,16 +1,53 @@
 import express from "express"
+// import { client } from "@repo/db/client"
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const app = express()
 
-app.post("/signup",(req,res)=>{
 
 
-    
-})
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+
+const model = genAI.getGenerativeModel({
+    model: "gemini-3-pro",
+});
+
+
+// app.post("/signup",async(req,res)=>{
+//     const { email, username, password } = req.body
+
+//     try{
+//         const response = await client.user.create({
+//             data:{
+//                 username : username,
+//                 email : email,
+//                 password : password
+//             }
+//         })
+//         res.status(200).json({
+//             id : response.id,
+//             msg : "user is successfully logged-in "
+//         })
+//     }catch(e){
+        
+//     }
+
+// })
 
 
 app.get("/repo",(req,res)=>{
     
+})
+
+app.get('/type', async(req,res)=>{
+    // const data =await model.generateContent(`
+    //         Your are model that generate a everytime new topic you user can type it for increase their typing speed 
+    //     `)
+
+    // const response = data.response.text
+    res.json({
+        typeData : "I am from Delhi"
+    })
 })
 
 app.listen(8000,()=>{
--- apps/websocket/index.ts
+++ apps/websocket/index.ts
@@ -1,6 +1,8 @@
+import axios from "axios";
 
 const content = "i am from Delhi"
-let ctr = 0
+const player = new Map() // store and current user<ws> 
+
 Bun.serve({
     port : 8080,
     fetch(req,serve){
@@ -14,31 +16,48 @@ Bun.serve({
     
     websocket:{
         
-        message(ws, msg){
+        async message(ws, msg){
             
             console.log("this msg came from the client -> ", msg)
             ws.send(content)
-            if(msg === content[ctr]){
-                ws.send(JSON.stringify({
-                    currentState : true,
-                    msg : "you're going very well" 
-                }))
-                ctr++
-            }else{
-                if(ctr >= content.length){
-                    ws.send(JSON.stringify({
-                        success : true,
-                        msg : "you've passed the test"
-                    }))
-                }
-                else{
-                    ws.send(JSON.stringify({
-                        currentState : false,
-                        msg : "you've sent wrong key"
-                    }))
-                }
-            }
-        }
-    }
 
-},)
+            player.set(ws, {
+                ctr : 0,
+                startTime : Date.now()
+            })
+        // console.log("player map -> ", player.get(ws).ctr)
+
+        const repsone = await axios.get('http://localhost:8000/type')
+        
+        if(msg == "kaif"){
+            player.set(ws,{
+                ctr : 5
+            })
+            ws.send(`you've sent kaif ${player.get(ws).ctr.toString()}`)
+            ws.send(`you have to type this ${JSON.stringify(repsone.data.typeData)}`)
+        }
+        // if(msg === content[ctr]){
+        //         ws.send(JSON.stringify({
+        //             currentState : true,
+        //             msg : "you're going very well" 
+        //         }))
+        //         ctr++
+        //     }else{
+        //         if(ctr >= content.length){
+        //             ws.send(JSON.stringify({
+        //                 success : true,
+        //                 msg : "you've passed the test"
+        //             }))
+        //         }
+        //         else{
+        //             ws.send(JSON.stringify({
+        //                 currentState : false,
+        //                 msg : "you've sent wrong key"
+        //             }))
+        //         }
+        //     }
+        // }
+        }
+
+    }
+})
