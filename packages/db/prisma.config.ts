66486a33b1ccef670d65fa812a9f009c211c2cf4
mohd kaif <mk2818356@gmail.com>
2026-03-18 23:52:10 +0530

    re-init db

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig, env } from "prisma/config";
+import 'dotenv/config'
+import { defineConfig, env } from 'prisma/config'
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: 'prisma/schema.prisma',
   migrations: {
-    path: "prisma/migrations",
+    path: 'prisma/migrations',
   },
-  engine: "classic",
   datasource: {
-    // Change this url for production 
-    url:"postgresql://postgres:mypassword@localhost:5432/mydb" 
+    url: env('DATABASE_URL'),
   },
-});
+})
